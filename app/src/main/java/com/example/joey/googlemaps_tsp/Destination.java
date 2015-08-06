0fccf76818d3bceaa37b88049f4b797ee6434382
Joey Lee <s3589@hotmail.com>
2015-08-05 23:40:45 -0700

    Corrected distanceTo (distance between destinations)
    android Location.distanceTo doesn't quite work. Switched to Haversine
    formula which seems to give accurate results.

--- app/src/main/java/com/example/joey/googlemaps_tsp/Destination.java
+++ app/src/main/java/com/example/joey/googlemaps_tsp/Destination.java
@@ -26,23 +26,15 @@ public class Destination {
         double lng1 = Math.toRadians(this.pos.longitude);
         double lat2 = Math.toRadians(destination.getLatLng().latitude);
         double lng2 = Math.toRadians(destination.getLatLng().longitude);
+        
+        // Haversine Formula (Distance of points on sphere)
+        double Radius = 6371; // radius of earth in km
 
-        Location loc1 = new Location("loc1");
-        loc1.setLatitude(lat1);
-        loc1.setLongitude(lng1);
-        Location loc2 = new Location("loc2");
-        loc2.setLatitude(lat2);
-        loc2.setLongitude(lng2);
-        return loc1.distanceTo(loc2);
-
-        // Alternative distance calcution: Haversine Formula (Distance of points on sphere)
-//        double Radius = 6371; // radius of earth in km
-//
-//        double dLat = lat2 - lat1;
-//        double dLng = lng2 - lng1;
-//        double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLng / 2),2) * Math.cos(lat1) * Math.cos(lat2);
-//        double c = 2 * Math.asin(Math.sqrt(a));
-//        return Radius * c;
+        double dLat = lat2 - lat1;
+        double dLng = lng2 - lng1;
+        double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLng / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return Radius * c;
     }
 
     public Marker getMarker() {
